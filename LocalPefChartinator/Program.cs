8e240c0a87f7884d50665a7bf1708085072953b1
Darius Lapūnas <d.lapunas@gmail.com>
2016-04-06 20:32:15 +0300

    Initial commit

---
+++ LocalPefChartinator/Program.cs
@@ -0,0 +1,42 @@
+﻿using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Svg;
+
+namespace LocalPefChartinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("out.svg", generate(null));
+        }
+
+        static string generate(IEnumerable<DataPoint> data)
+        {
+            var document = new SvgDocument()
+            {
+                Width = 100,
+                Height = 100
+            };
+            document.Children.Add(new SvgCircle() { CenterX = 50, CenterY = 50, Radius = 40, str });
+            
+        }
+    }
+
+    struct DataPoint
+    {
+        public readonly int pef;
+        public readonly Instant time;
+
+        public DataPoint(int pef, Instant time)
+        {
+            this.pef = pef;
+            this.time = time;
+        }
+    }
+}
