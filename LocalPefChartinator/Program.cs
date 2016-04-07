8c63857a33b7612284fd01611b53526a755d78c2
Darius Lapūnas <d.lapunas@gmail.com>
2016-04-07 15:04:50 +0300

    Basic structure

--- LocalPefChartinator/Program.cs
+++ LocalPefChartinator/Program.cs
@@ -8,13 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Svg;
+using Svg.Transforms;
 
 namespace LocalPefChartinator
 {
     public class Program
     {
-        private const int Width = 800;
-        private const int Height = 700;
+        private static readonly SvgColourServer LineColor = new SvgColourServer(Color.Blue);
+        private const int DayWidth = 50;
+        private const int SideColumnWidth = 60;
+        private const int DaysPerWeek = 7;
+        private const int Weeks = 3;
+        private const int TopRow = 70;
+        private const int MaxPef = 710;
+        private const float PefSize = 1f;
+        private const int DayRow = 230;
+        private const int DateRow = 230;
+
+        private const float ChartHeight = MaxPef * PefSize;
+        private const float ChartWidth = Weeks*DaysPerWeek*DayWidth;
+
+        private const float Width = ChartWidth + SideColumnWidth * 2;
+        private const float Height = TopRow + ChartHeight + DayRow + DateRow;
         private const int Padding = 10;
 
         public static void Main(string[] args)
@@ -27,19 +42,109 @@ namespace LocalPefChartinator
             var document = new SvgDocument
             {
                 Width = Width + Padding * 2,
-                Height = Height + Padding * 2,
-                ViewBox = new SvgViewBox(-Padding, -Padding, Width, Height)
+                Height = Height + Padding * 2
             };
-            document.Children.Add(
-                new SvgRectangle()
-                {   
-                    Width = Width,
-                    Height =  Height,
-                    Fill =  new SvgColourServer(Color.Blue),
-                    Stroke = new SvgColourServer(Color.Aqua),
-                    StrokeWidth = new SvgUnit(2)
-                });
+            var contentGroup = new SvgGroup()
+            {
+                Transforms = new SvgTransformCollection {new SvgTranslate(Padding, Padding)}
+            };
+            contentGroup.Children.Add(new SvgRectangle()
+            {   
+                Width = Width,
+                Height =  Height,
+                Fill =  new SvgColourServer(Color.Transparent),
+                Stroke = LineColor
+            });
+            contentGroup.Children.Add(new SvgLine()
+            {
+                StartX = SideColumnWidth,
+                StartY = 0,
+                EndX = SideColumnWidth,
+                EndY = Height,
+                Stroke = LineColor
+            });
+            contentGroup.Children.Add(new SvgLine()
+            {
+                StartX = Width - SideColumnWidth,
+                StartY = 0,
+                EndX = Width - SideColumnWidth,
+                EndY = Height,
+                Stroke = LineColor
+            });
+            contentGroup.Children.Add(GetTopGroup());
+            contentGroup.Children.Add(GetChartGroup());
+            contentGroup.Children.Add(GetDayGroup());
+            contentGroup.Children.Add(GetDateGroup());
+            document.Children.Add(contentGroup);
             return document.GetXML();
+        }
+
+        private static SvgElement GetTopGroup()
+        {
+            SvgGroup group = new SvgGroup()
+            {
+                Transforms = new SvgTransformCollection() { new SvgTranslate(SideColumnWidth, 0)}
+            };
+            group.Children.Add(new SvgLine()
+            {
+                StartX = 0,
+                StartY = TopRow,
+                EndX = ChartWidth,
+                EndY = TopRow,
+                Stroke = LineColor
+            });
+            return group;
+        }
+
+        private static SvgElement GetChartGroup()
+        {
+            SvgGroup group = new SvgGroup()
+            {
+                Transforms = new SvgTransformCollection() {new SvgTranslate(SideColumnWidth, TopRow)}
+            };
+            group.Children.Add(new SvgLine()
+            {
+                StartX = 0,
+                StartY = ChartHeight,
+                EndX = ChartWidth,
+                EndY = ChartHeight,
+                Stroke = LineColor
+            });
+            return group;
+        }
+
+        private static SvgElement GetDayGroup()
+        {
+            SvgGroup group = new SvgGroup()
+            {
+                Transforms = new SvgTransformCollection() { new SvgTranslate(SideColumnWidth, TopRow + ChartHeight)}
+            };
+            group.Children.Add(new SvgLine()
+            {
+                StartX = 0,
+                StartY = DayRow,
+                EndX = ChartWidth,
+                EndY = DayRow,
+                Stroke = LineColor
+            });
+            return group;
+        }
+
+        private static SvgElement GetDateGroup()
+        {
+            SvgGroup group = new SvgGroup()
+            {
+                Transforms = new SvgTransformCollection() { new SvgTranslate(SideColumnWidth, TopRow + ChartHeight + DayRow) }
+            };
+            group.Children.Add(new SvgLine()
+            {
+                StartX = 0,
+                StartY = DateRow,
+                EndX = ChartWidth,
+                EndY = DateRow,
+                Stroke = LineColor
+            });
+            return group;
         }
     }
 
