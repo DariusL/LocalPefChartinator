0cd224e859668240e8ffef5e3daa2d6fd096baf9
Darius Lapūnas <d.lapunas@gmail.com>
2016-04-08 15:52:37 +0300

    Pull out chart code from Program.cs

---
+++ LocalPefChartinator/DataPoint.cs
@@ -0,0 +1,16 @@
+﻿using NodaTime;
+
+namespace LocalPefChartinator
+{
+    public struct DataPoint
+    {
+        public readonly int Pef;
+        public readonly ZonedDateTime Time;
+
+        public DataPoint(int pef, ZonedDateTime time)
+        {
+            Pef = pef;
+            Time = time;
+        }
+    }
+}
