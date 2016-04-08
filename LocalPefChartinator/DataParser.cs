02692065d1ea6e5583db1e7c97a087a03b27aae3
Darius Lapūnas <d.lapunas@gmail.com>
2016-04-08 16:16:36 +0300

    Start parsing

---
+++ LocalPefChartinator/DataParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodaTime;
+
+namespace LocalPefChartinator
+{
+    class DataParser
+    {
+        public static IReadOnlyList<DataPoint> Parse(IReadOnlyList<Tuple<string, string>> data, DateTimeZone timezone)
+        {
+            return data.Select(tuple => Parse(tuple, timezone)).ToArray();
+        }
+
+        private static DataPoint Parse(Tuple<string, string> tuple, DateTimeZone timezone)
+        {
+            long time = long.Parse(tuple.Item1);
+            Instant instant = Instant.FromSecondsSinceUnixEpoch(time);
+            int pef = int.Parse(tuple.Item2);
+            return new DataPoint(pef, new ZonedDateTime(instant, timezone));
+        }
+    }
+}
--- LocalPefChartinator/Program.cs
+++ LocalPefChartinator/Program.cs
@@ -30,16 +30,4 @@ namespace LocalPefChartinator
             ));
         }
     }
-
-    internal struct DataPoint
-    {
-        public readonly int Pef;
-        public readonly ZonedDateTime Time;
-
-        public DataPoint(int pef, ZonedDateTime time)
-        {
-            Pef = pef;
-            Time = time;
-        }
-    }
 }
