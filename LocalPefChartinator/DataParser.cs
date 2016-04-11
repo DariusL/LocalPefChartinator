26007bcbc7a510fbb3b12002a036240a8fb410a5
Darius Lapūnas <d.lapunas@gmail.com>
2016-04-11 14:52:28 +0300

    Add timezone parsing

--- LocalPefChartinator/DataParser.cs
+++ LocalPefChartinator/DataParser.cs
@@ -1,25 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using NodaTime;
+using NodaTime.Text;
+using NodaTime.TimeZones;
 
 namespace LocalPefChartinator
 {
-    class DataParser
+    public class DataParser
     {
-        public static IReadOnlyList<DataPoint> Parse(IReadOnlyList<Tuple<string, string>> data, DateTimeZone timezone)
+        public static IReadOnlyList<DataPoint> Parse(IReadOnlyList<Tuple<string, string>> data, string timezone)
         {
-            return data.Select(tuple => Parse(tuple, timezone)).ToArray();
+            DateTimeZone tz = ParseTimeZone(timezone);
+            return data.Select(item => Parse(item, tz)).ToArray();
         }
 
         private static DataPoint Parse(Tuple<string, string> tuple, DateTimeZone timezone)
         {
-            long time = long.Parse(tuple.Item1);
+            long time = Int64.Parse(tuple.Item1);
             Instant instant = Instant.FromSecondsSinceUnixEpoch(time);
             int pef = int.Parse(tuple.Item2);
             return new DataPoint(pef, new ZonedDateTime(instant, timezone));
+        }
+
+        public static TzdbZoneLocation ParseLocale(string locale)
+        {
+            return locale.Contains("_") ? ParseLocation(locale.Substring(3, 2)) : null;
+        }
+
+        public static TzdbZoneLocation ParseLocation(string locationString)
+        {
+            TzdbDateTimeZoneSource timeZoneSource = TzdbDateTimeZoneSource.Default;
+            return
+                timeZoneSource.ZoneLocations.First(
+                    location => location.CountryCode.Equals(locationString, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static ZonedDateTime ParseIsoTime(string isoTimeString)
+        {
+            return ZonedDateTime.FromDateTimeOffset(DateTimeOffset.Parse(isoTimeString, CultureInfo.InvariantCulture));
+        }
+
+        public static DateTimeZone ParseTimeZone(string zone)
+        {
+            return DateTimeZone.ForOffset(OffsetPattern.GeneralInvariantPatternWithZ.Parse(zone).Value);
         }
     }
 }
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using LocalPefChartinator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NodaTime;
+using NodaTime.TimeZones;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFindLithuaniaFromLocation()
+        {
+            var lithuania = DataParser.ParseLocation("LT");
+            Assert.AreEqual("Lithuania", lithuania.CountryName);
+        }
+
+        [TestMethod]
+        public void TestFindLithuaniaFromLocale()
+        {
+            var lithuania = DataParser.ParseLocale("lt_LT");
+            Assert.AreEqual("Lithuania", lithuania.CountryName);
+        }
+
+        [TestMethod]
+        public void TestParseTime()
+        {
+            DataParser.ParseIsoTime("2008-09-15T15:53:00+05:00");
+        }
+
+        [TestMethod]
+        public void TestParseTimeZone()
+        {
+            Action<int, int, string> test = (hours, minutes, str) =>
+            {
+                Assert.AreEqual(Zone(hours, minutes), DataParser.ParseTimeZone(str));
+            };
+
+            test(0, 0, "Z");
+            test(1, 0, "+01:00");
+            test(-1, 0, "-01:00");
+            test(1, 0, "+01");
+            test(1, 0, "+01:00");
+            test(-2, -30, "-02:30");
+        }
+
+        private static DateTimeZone Zone(int hours, int minutes)
+        {
+            return DateTimeZone.ForOffset(Offset.FromHoursAndMinutes(hours, minutes));
+        }
+    }
+}
